d647a4923018f1f63e23a9cdee88bcfc83537fd2
medlynhan <midiline30@gmail.com>
2025-07-24 19:41:51 +0700

    finish

--- src/components/ProfileImage.jsx
+++ src/components/ProfileImage.jsx
@@ -10,32 +10,15 @@ export default function ProfileImage() {
   }
 
   return (
-    <div className='image-area cursor-pointer z-10'>
-      {/* Gambar kecil yang dapat diklik */}
-      <Image
-        src="/profile.png"
-        alt="Madeline Profile"
-        width={100}
-        height={100}
-        className="rounded-full z-10"
-        onClick={scaleImage} // Membuka gambar besar saat gambar kecil di-klik
-      />
+    <div className='image-area cursor-pointer z-50'>
+      <Image src="/profile.png" alt="Madeline Profile" width={100} height={100} className="rounded-full" onClick={scaleImage} />
 
-      {/* Modal gambar besar jika `image` true */}
       {image && (
-        <div className="fixed top-0 left-0 w-full h-full bg-black bg-opacity-50 flex justify-center items-center z-50" onClick={scaleImage}>
-          
-          
-          {/* Gambar besar yang ditampilkan */}
-          <Image
-            src="/profile.png"
-            alt="Madeline Profile"
-            width={400}
-            height={400}
-            className="rounded-full z-50"
-          />
+        <div className="fixed top-0 left-0 w-full h-full bg-stone-950/70 flex justify-center items-center" onClick={scaleImage}>
+          <Image src="/profile.png" alt="Madeline Profile" width={300} height={300} className="rounded-full "/>
         </div>
       )}
     </div>
+
   );
 }
--- src/components/Toogle.jsx
+++ src/components/Toogle.jsx
@@ -1,33 +1,33 @@
 'use client';
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 export default function Toogle() {
 
-    let emojiToggle = "";
 
     const [isDay, setIsDay] = useState(false);
-    
+    const [emojiToggle, setEmojiToggle] = useState("🌙");
     function toggleClick() {
         setIsDay(!isDay);
     }
 
-    if (isDay === true){
-        emojiToggle = "☀️";
-        document.documentElement.style.setProperty("--white", "#fdfcfc");
-        document.documentElement.style.setProperty("--black", "#121212 ");
-        document.documentElement.style.setProperty("--gray", "#303030");
-        document.documentElement.style.setProperty("--yellow", "#ffd88f");
-    }else{
-        emojiToggle = "🌙";
-        document.documentElement.style.setProperty("--white", "#121212 ");
-        document.documentElement.style.setProperty("--black", "#fdfcfc");
-        document.documentElement.style.setProperty("--gray", "#d9d9d9ff");
-        document.documentElement.style.setProperty("--yellow", "#e0a73cff");
-    }
-    
+    useEffect(() => {
+        if (isDay === true){
+            setEmojiToggle("☀️");
+            document.documentElement.style.setProperty("--white", "#fdfcfc");
+            document.documentElement.style.setProperty("--black", "#121212 ");
+            document.documentElement.style.setProperty("--gray", "#303030");
+            document.documentElement.style.setProperty("--yellow", "#ffd88f");
+        }else{
+            setEmojiToggle("🌙");
+            document.documentElement.style.setProperty("--white", "#121212 ");
+            document.documentElement.style.setProperty("--black", "#fdfcfc");
+            document.documentElement.style.setProperty("--gray", "#d9d9d9ff");
+            document.documentElement.style.setProperty("--yellow", "#e0a73cff");
+        }
+    }, [isDay]);
 
     return (
-        <p className="z-40 cursor-pointer fixed absolute top-2 sm:right-1 lg:left-1 bg-transparent font-mono" onClick={toggleClick}><span className='border border-(--yellow) rounded-full p-1 py-1.5'>{emojiToggle}</span></p>
+        <p className="z-40 cursor-pointer fixed absolute top-2 sm:right-1 bg-transparent font-mono" onClick={toggleClick}><span className='border border-(--yellow) rounded-full p-0.5 py-1'>{emojiToggle}</span></p>
       
     )
 }
