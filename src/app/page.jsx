85ac2f06f1cec45a74c5578ede82201e3abd39d2
medlynhan <midiline30@gmail.com>
2025-09-15 15:32:48 +0700

    finish home page

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -5,12 +5,86 @@ import Icon from "../components/Icon";
 import Toogle from "../components/Toogle";
 import ProfileImage from "../components/ProfileImage";
 import { DiVim } from "react-icons/di";
+import { useState, useRef , useEffect} from "react";
+import { motion } from "framer-motion";
+import { IoCaretForwardOutline } from "react-icons/io5";
 
 export default function Home() {
+
+  const [aboutclicked,SetAboutClicked] = useState(false);
+  const [teamclicked,SetTeamClicked] = useState(false);
+  const [individualclicked,SetIndividualClicked] = useState(false);
+
+  const aboutRef = useRef(null);
+  const teamRef = useRef(null);
+  const individualRef = useRef(null);
+
+  const scrollToSection = (ref) => {
+    if (ref.current) {
+      const top = ref.current.getBoundingClientRect().top + window.scrollY;
+      window.scrollTo({
+        top: top , // offset global -40
+        behavior: "smooth",
+      });
+
+      if (ref.current === aboutRef.current) {
+         SetAboutClicked(true);
+         SetTeamClicked(false);
+         SetIndividualClicked(false);
+      }
+
+      if (ref.current === teamRef.current) {
+         SetAboutClicked(false);
+         SetTeamClicked(true);
+         SetIndividualClicked(false);
+      }
+
+      if (ref.current === individualRef.current) {
+         SetAboutClicked(false);
+         SetTeamClicked(false);
+         SetIndividualClicked(true);
+      }
+
+
+    }
+  };
+
+useEffect(() => {
+  const handleScroll = () => {
+    const aboutSection = aboutRef.current.getBoundingClientRect();
+    const teamSection = teamRef.current.getBoundingClientRect();
+    const individualSection = individualRef.current.getBoundingClientRect();
+
+    
+    // Check if the section is in the viewport
+    if (aboutSection.top - 50 <= (window.innerHeight / 2) && aboutSection.bottom >= 0) {
+      SetAboutClicked(true);
+      SetTeamClicked(false);
+      SetIndividualClicked(false);
+    } else if (teamSection.top - 50 <= (window.innerHeight / 2)  && teamSection.bottom >= 0) {
+      SetAboutClicked(false);
+      SetTeamClicked(true);
+      SetIndividualClicked(false);
+    } else if (individualSection.top - 50 <= (window.innerHeight / 2) && individualSection.bottom >= 0) {
+      SetAboutClicked(false);
+      SetTeamClicked(false);
+      SetIndividualClicked(true);
+    }
+  };
+
+  window.addEventListener('scroll', handleScroll);
+
+  // Cleanup on component unmount
+  return () => {
+    window.removeEventListener('scroll', handleScroll);
+  };
+}, []);
+
+  
   return (
     <div >
       {/*kolom kiri */}
-      <div className="introduction-container lg:w-[35%] lg:absolute lg:left-0 lg:top-0 lg:fixed lg:ml-[5%] lg:my-[5%]">
+      <div className="  lg:min-h-[80vh] introduction-container lg:w-[35%] lg:absolute lg:left-0 lg:top-0 lg:fixed lg:ml-[5%] lg:my-[5%]">
         <div className="gap-4   w-full flex  flex-row h-full">
             <div className="flex justify-center items-center w-fit">
               <ProfileImage></ProfileImage>
@@ -29,32 +103,46 @@ export default function Home() {
               </div>
             </div>
         </div>
-        <div className="w-full hidden lg:flex flex-row justify-between items-center pt-2 border-t-1 border-gray-700 ">
+        <div className=" lg:flex hidden gap-4  h-full min-h-[30vh] flex flex-col justify-center ">
+            <div className={`flex items-center gap-2 transition duration-150 cursor-pointer hover:text-(--yellow)  ${ aboutclicked ? "text-(--yellow)" : " "}`}>
+              <p className={`text-(--white) transition duration-150 ${ aboutclicked ? "text-(--yellow)" : "text-(--white)"}`}><IoCaretForwardOutline /></p>
+              <p onClick={() => scrollToSection(aboutRef)}> <span></span>About Me</p>
+            </div>
+            <div className={`flex items-center gap-2 transition duration-150 cursor-pointer hover:text-(--yellow)  ${ teamclicked ? "text-(--yellow)" : " "}`}>
+              <p className={`text-(--white) transition duration-150 ${ teamclicked ? "text-(--yellow)" : "text-(--white)"}`}><IoCaretForwardOutline /></p>
+              <p onClick={() => scrollToSection(teamRef)}> <span></span>Team Projects</p>
+            </div>
+            <div className={`flex items-center gap-2 transition duration-150 cursor-pointer hover:text-(--yellow)  ${ individualclicked ? "text-(--yellow)" : " "}`}>
+              <p className={`text-(--white) transition duration-150 ${ individualclicked ? "text-(--yellow)" : "text-(--white)"}`}><IoCaretForwardOutline /></p>
+              <p onClick={() => scrollToSection(individualRef)}> <span></span>Individual Projects</p>
+            </div>            
+        </div>
+        <div className="w-full hidden lg:flex flex-row justify-between items-center pt-2  ">
             <Icon></Icon>
             {/* <div className="flex  gap-2 items-center px-2 py-1 border-1 border-(--black) text-(--black) rounded-full hover:border-(--yellow) hover:text-(--yellow) cursor-pointer transition duration-300 ease-in-out">
               <p>View CV </p>
               <p className="hover:-translate-y-1">↗</p>
             </div> */}
-          </div>
+        </div>
       </div>
       
       {/*kolom kanan */}
-      <div className="projects-container lg:w-[55%] lg:absolute lg:right-0 lg:top-0 lg:mr-[5%] lg:my-[5%]">
+      <div className="projects-container lg:w-[55%] lg:absolute lg:right-0 lg:top-0 lg:mr-[5%] lg:my-[5%] text-gray-200">
       
-        <div className="flex flex-col gap-6 lg:min-h-[90vh] pb-5">
+        <div ref={aboutRef} className="flex flex-col gap-6 lg:min-h-[90vh] py-10 ">
             <p className="sub-header-text lg:text-lg font-semibold ">About Me</p>
             <p className="text-justify">
-              Hey, I’m a Computer Science student at BINUS University. <span className="text-(--black) font-semibold hover:text-(--yellow) cursor-pointer">I love exploring different areas across the fullstack</span>, from backend logic with Node.js and Laravel to frontend design with React, Next.js, and TailwindCSS. Besides studying, sometimes I also take on freelance projects !
+              Hey, I’m Madeline, a Computer Science student at BINUS University. <span className="text-(--black) font-semibold hover:text-(--yellow) cursor-pointer">I love exploring different areas across the fullstack</span>, from backend logic with Node.js and Laravel to frontend design with React, Next.js, and TailwindCSS. Besides studying, sometimes I also take on freelance projects !
             </p>
             <p className="text-justify">
-              I enjoy building digital products end-to-end, but I especially find myself <span className="text-(--black) font-semibold hover:text-(--yellow) cursor-pointer">drawn to the frontend</span> — where I get to turn ideas into beautiful and interactive experiences. 💗 
+              I enjoy building digital products end-to-end, but I especially find myself <span className="text-(--black) font-semibold hover:text-(--yellow) cursor-pointer">drawn to the frontend</span> — where I get to turn ideas into beautiful and interactive experiences. 
             </p>
             <p className="text-justify">
               Here, I’ll show you some of my work from <span className=" text-(--black) font-semibold hover:text-(--yellow) cursor-pointer">competitions, team projects, and personal explorations.</span> I’m also active on <span className=" text-(--black) font-semibold hover:text-(--yellow) cursor-pointer">GitHub</span>, where I regularly share updates and new projects — feel free to check it out and follow along on my journey! ✨🚀
             </p>
         </div> 
           
-        <div className="flex flex-col gap-6 lg:min-h-[90vh] pb-5">
+        <div ref={teamRef} className="flex flex-col gap-6 lg:min-h-[90vh] py-10">
             <p className="sub-header-text lg:text-lg font-semibold ">Team Projects</p>
             <Project 
                 link={"https://leluri.vercel.app/"} 
@@ -66,7 +154,7 @@ export default function Home() {
             />
         </div>
           
-        <div className="flex flex-col gap-6  lg:min-h-[90vh] pb-5">
+        <div ref={individualRef} className="flex flex-col gap-6  lg:min-h-[90vh] py-10">
             <p className="sub-header-text lg:text-lg font-semibold ">Individual Projects</p>
             <Project 
                 link={"https://tapaknusa.up.railway.app/"} 
